0f5764d0527532f695c650ce63c296453372032d
Amma Obeid <amfo250@gmail.com>
2025-06-24 19:25:17 +0200

    GeoLocation

---
+++ GeoLocation/.history/script_20250624163547.js
@@ -0,0 +1,11 @@
+const map = L.map('map').setView([0, 0], 2);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+const marker = L.marker([0, 0]).addTo(map);
+navigator.geolocation.getCurrentPosition(function (pos) {
+    const 
+})
